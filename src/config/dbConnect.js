bc97c833e14669b631a1a5e094c750a9353cb104
Junior Moritz <33938605+johwboy@users.noreply.github.com>
2022-06-27 21:39:34 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://los:123@cluster0.a9kyeyr.mongodb.net/alura-node?retryWrites=true&w=majority",
+mongoose.connect("mongodb+srv://user:password@cluster0.a9kyeyr.mongodb.net/alura-node?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true })
 
 let db = mongoose.connection
